e306b73e104633d19315bab9a101579ae87dd193
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-12-06 18:06:59 -0800

    WK8Lab2 – Recursive Function Lab Submissions

--- WK8Lab2___Recursive_Function.py
+++ WK8Lab2___Recursive_Function.py
@@ -1,1 +1,20 @@
+#Jamie Johnson
+#CIS261
+#WK8Lab2 – Recursive Function Lab Submissions
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else: 
+        return fib(n - 1) + fib(n - 2)
+
+def main():
+    for i in range(16):
+        print(fib(i), end=", ")
+    print("...")
+
+if __name__ == "__main__":
+    main()
 
